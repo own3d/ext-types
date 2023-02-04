80d338d4dc4886869346b43e469b103c41162087
René Preuß <rene@preuss.io>
2023-02-04 12:49:36 +0100

    Add documentation

--- own3d-ext.d.ts
+++ own3d-ext.d.ts
@@ -1,7 +1,38 @@
+/**
+ * Type Definitions for OWN3D Extensions.
+ *
+ * @see https://dev.stream.tv/docs/
+ */
 declare namespace OWN3D.ext {
+    /**
+     * The current version of the extension API.
+     */
     const version: string;
+
+    /**
+     * The current environment of the extension API.
+     */
     const environment: 'production';
+
+    /**
+     * A socket is basically an EventEmitter which sends events
+     * to — and receive events from — the server over the network.
+     */
+    namespace socket {
+        /**
+         * Register a callback to be invoked when a specific event is emitted.
+         */
+        function on(event: string, callback: (data: any) => void): void;
+    }
+
+    /**
+     * Register a callback to be invoked when the extension is authorized.
+     */
     function onAuthorized(authCallback: (auth: Authorized) => void): void;
+
+    /**
+     * Register a callback to be invoked when the extension context changes.
+     */
     function onContext(
         contextCallback: <T extends Partial<Context>>(context: T, changed: ReadonlyArray<keyof T>) => void,
     ): void;
